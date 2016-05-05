26db8adce1429420631401b61908ef1e54cf5bab
KarinaDatore <karina.datore@fatec.sp.gov.br>
2016-05-05 20:27:37 -0300

    Update cadastrar_indeferimento.php
    Inclusão Campos Obrigatórios

--- application/views/indeferimento/cadastrar_indeferimento.php
+++ application/views/indeferimento/cadastrar_indeferimento.php
@@ -8,7 +8,7 @@
             echo "<div class='form-group'>";
             echo form_label("Motivo do Indeferimento", "motivoInd", array("class" => "col-sm-2 control-label"));
             echo "<div class='col-sm-6'>";
-            echo form_input(array("name" => "motivoInd", "id" => "motivoInd" ,"class" => "form-control", "maxlength" => "40"));
+            echo form_input(array("name" => "motivoInd", "required" => "required","id" => "motivoInd" ,"class" => "form-control", "maxlength" => "40"));
             echo "</div>";
             echo "</div>";
             echo "<div class='form-group'>";
