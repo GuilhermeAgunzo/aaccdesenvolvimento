a7b2dba69dc366c48bb0c197affb5c4298784b88
KarinaDatore <karina.datore@fatec.sp.gov.br>
2016-05-05 20:29:21 -0300

    Update pesquisar_indeferimento.php
    Inclusão do dropdown para pesquisa e campos Obrigatórios

--- application/views/indeferimento/pesquisar_indeferimento.php
+++ application/views/indeferimento/pesquisar_indeferimento.php
@@ -2,12 +2,14 @@
 echo form_fieldset("<h1>Pesquisa do Motivo de Indeferimento</h1>");
 
 
+
 $atributos = array('class' => 'form-horizontal');
 echo form_open('email/send', $atributos);
 echo "<div class='form-group'>";
 echo form_label("Motivo do Indeferimento", "motivoInd", array("class" => "col-sm-2 control-label"));
-echo "<div class='col-sm-6'>";
-echo form_input(array("name" => "motivoInd", "id" => "motivoInd" ,"class" => "form-control", "maxlength" => "80"));
+echo "<div class='col-sm-10'>";
+$nmMotivoIndeferimento = array('Motivo do Indeferimento' => 'Selecione o Motivo','motivo1' => 'Motivo 1','motivo2' => 'Motivo 2');
+echo form_dropdown('MotivoIndeferimento', $nmMotivoIndeferimento, array("class" => "form-control"));
 echo "</div>";
 echo "</div>";
 
@@ -18,5 +20,4 @@ echo "</div>";
 echo "</div>";
 
 echo form_close();
-
 ?>
