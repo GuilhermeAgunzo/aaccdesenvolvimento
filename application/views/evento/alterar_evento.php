fe40a54d01ce2969ba2963c5da5fb060a827f6ad
KarinaDatore <karina.datore@fatec.sp.gov.br>
2016-05-08 10:22:26 -0300

    Update alterar_evento.php
    Alteração do Cadastro de Evento, Inclusão dos Campos de Pesquisa por Data Inicial do Evento e Data Final do Evento

--- application/views/evento/alterar_evento.php
+++ application/views/evento/alterar_evento.php
@@ -2,12 +2,18 @@
 echo form_fieldset("<h1>Alteração de Evento</h1>");
 
 $atributos = array('class' => 'form-horizontal');
-echo form_open('email/send', $atributos);
+
 echo "<div class='form-group'>";
-echo form_label("Título do Evento", "tituloDoEvento", array("class" => "col-sm-2 control-label"));
-echo "<div class='col-sm-10'>";
-$nmEvento = array('Nome do Evento' => 'Selecione o Evento','evento1' => 'Evento 1','evento2' => 'Evento 2');
-echo form_dropdown('NomedoEvento', $nmEvento, array("class" => "form-control"));
+echo form_label("Data do Evento", "dataDoEvento", array("class" => "col-sm-2 control-label"));
+echo "<div class='col-sm-4'>";
+echo form_input(array("name" => "dtEvento","required" => "required","type" => "date", "id" => "dtEvento" ,"class" => "form-control", "maxlength" => "10"));
+echo "</div>";
+echo "</div>";
+
+echo "<div class='form-group'>";
+echo form_label("Data Final do Evento", "dataFinalDoEvento", array("class" => "col-sm-2 control-label"));
+echo "<div class='col-sm-4'>";
+echo form_input(array("name" => "dtFinalEvento","required" => "required","type" => "date", "id" => "dtFinalEvento" ,"class" => "form-control", "maxlength" => "10"));
 echo "</div>";
 echo "</div>";
 
