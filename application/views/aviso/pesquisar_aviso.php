9698981dc4bcead167bfbe46e9fad4c354f49f4b
KarinaDatore <karina.datore@fatec.sp.gov.br>
2016-05-08 11:47:37 -0300

    Update pesquisar_aviso.php
    Inclusão dos campos de Data Inicial e Data Final para Cadastro de Aviso.

--- application/views/aviso/pesquisar_aviso.php
+++ application/views/aviso/pesquisar_aviso.php
@@ -3,11 +3,18 @@ echo form_fieldset("<h1>Pesquisa de Avisos</h1>");
 
 $atributos = array('class' => 'form-horizontal');
 echo form_open('email/send', $atributos);
+
 echo "<div class='form-group'>";
-echo form_label("Título", "titulo_aviso", array("class" => "col-sm-2 control-label"));
-echo "<div class='col-sm-10'>";
-$nmAviso = array('Nome do Aviso' => 'Selecione o Aviso','aviso1' => 'Aviso 1','aviso2' => 'Aviso 2');
-echo form_dropdown('NomedoAviso', $nmAviso, array("class" => "form-control"));
+echo form_label("Data Inicial do Aviso", "dataInicialDoAviso", array("class" => "col-sm-2 control-label"));
+echo "<div class='col-sm-4'>";
+echo form_input(array("name" => "dtInicialAviso","required" => "required","type" => "date", "id" => "dtInicialAviso" ,"class" => "form-control", "maxlength" => "10"));
+echo "</div>";
+echo "</div>";
+
+echo "<div class='form-group'>";
+echo form_label("Data Final do Aviso", "dataFinalDoAviso", array("class" => "col-sm-2 control-label"));
+echo "<div class='col-sm-4'>";
+echo form_input(array("name" => "dtFinalAviso","required" => "required","type" => "date", "id" => "dtFinalAviso" ,"class" => "form-control", "maxlength" => "10"));
 echo "</div>";
 echo "</div>";
 
@@ -16,7 +23,6 @@ echo "<div class='col-sm-offset-2 col-sm-10'>";
 echo form_button(array("class" => "btn btn-default", "content" => "Pesquisar", "type" => "submit"));
 echo "</div>";
 echo "</div>";
-
 
 echo form_close();
 ?>
