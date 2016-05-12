785ea19bd2b0c50eb0243d005380dbd7c73c2ab6
Caio Lage <caio.jorge@fatec.sp.gov.br>
2016-05-12 16:47:00 -0300

    removendo duplicatas

--- application/views/professor2/anexo_documento_comprobatorio.php
+++
@@ -1,5 +0,0 @@
-            <?php
-
-            echo form_fieldset("<h1>Anexo de Documento Comprobatórior</h1>");
-
-            ?>
--- application/views/professor2/relatorio_protocolo_entregas.php
+++
@@ -1,4 +0,0 @@
-            <?php
-            echo form_fieldset("<h1>Relatório de Protocolo de Entregas</h1>");
-
-            ?>
