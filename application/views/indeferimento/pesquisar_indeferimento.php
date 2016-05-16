eb4df18a2c75d615501e6c51383e7a040e2c1dee
KarinaDatore <karina.datore@fatec.sp.gov.br>
2016-05-16 10:34:44 -0300

    Update pesquisar_indeferimento.php
    Pesquisa do Motivo do Indeferimento, modelo de grid de Pesquisa

--- application/views/indeferimento/pesquisar_indeferimento.php
+++ application/views/indeferimento/pesquisar_indeferimento.php
@@ -2,20 +2,27 @@
 echo form_fieldset("<h1>Pesquisa do Motivo de Indeferimento</h1>");
 
 
-
 $atributos = array('class' => 'form-horizontal');
 echo form_open('email/send', $atributos);
 echo "<div class='form-group'>";
-echo form_label("Motivo do Indeferimento", "motivoInd", array("class" => "col-sm-2 control-label"));
 echo "<div class='col-sm-10'>";
-$nmMotivoIndeferimento = array('Motivo do Indeferimento' => 'Selecione o Motivo','motivo1' => 'Motivo 1','motivo2' => 'Motivo 2');
-echo form_dropdown('MotivoIndeferimento', $nmMotivoIndeferimento, array("class" => "form-control"));
-echo "</div>";
-echo "</div>";
-
-echo "<div class='form-group'>";
-echo "<div class='col-sm-offset-2 col-sm-10'>";
-echo form_button(array("class" => "btn btn-default", "content" => "Pesquisar", "type" => "submit"));
++echo "<table border='1'>";
+ +echo "<tr>";
+ +echo "<td>Motivo do Indeferimento 1</td>";
+ +echo "</tr>";
+ +echo "<tr>";
+ +echo "<td>Motivo do Indeferimento 2</td>";
+ +echo "</tr>";
+ +echo "<tr>";
+ +echo "<td>Motivo do Indeferimento 3</td>";
+ +echo "</tr>";
+ +echo "<tr>";
+ +echo "<td>Motivo do Indeferimento 4</td>";
+ +echo "</tr>";
+ +echo "<tr>";
+ +echo "<td>Motivo do Indeferimento 5</td>";
+ +echo "</tr>";
+ +echo "</table>";
 echo "</div>";
 echo "</div>";
 
