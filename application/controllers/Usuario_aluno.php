a2fe7dfe9330ff51ca37db8d2c5ef109effc16a4
Guilherme Agunzo <g.agunzo@gmail.com>
2016-09-23 10:50:38 -0300

    Aluno deve trocar senha no primeiro acesso

--- application/controllers/Usuario_aluno.php
+++ application/controllers/Usuario_aluno.php
@@ -11,46 +11,46 @@ class Usuario_aluno extends CI_Controller
 
     public function perguntas_frequentes()
     {
-
+        autoriza(1);
         $this->load->template_usuario_aluno("usuario_aluno/perguntas_frequentes_aluno.php");
 
     }
 
     public function tutorial_aluno()
     {
-
+        autoriza(1);
         $this->load->template_usuario_aluno("usuario_aluno/tutorial_aluno.php");
 
     }
 
     public function relatorio_horas()
     {
-
+        autoriza(1);
         $this->load->template_usuario_aluno("usuario_aluno/relatorio_horas_aluno.php");
 
     }
 
     public function pesquisa_horas()
     {
-
+        autoriza(1);
         $this->load->template_usuario_aluno("usuario_aluno/pesquisa_horas_concluidas.php");
 
     }
     public function cadastrar_relatorio()
     {
-
+        autoriza(1);
         $this->load->template_usuario_aluno("usuario_aluno/cadastrar_relatorio.php");
 
     }
      public function alterar_relatorio()
     {
-
+        autoriza(1);
         $this->load->template_usuario_aluno("usuario_aluno/alterar_relatorio.php");
 
     }
      public function visualizar_relatorio()
     {
-
+        autoriza(1);
         $this->load->template_usuario_aluno("usuario_aluno/visualizar_relatorio.php");
 
     }
