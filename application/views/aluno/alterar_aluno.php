db68fcc93efcd04d78afe4f8b27d58b4689e16eb
victorcsandrade <victorcsandrade@gmail.com>
2016-09-25 20:15:50 -0300

    Correção altera aluno desativado

--- application/views/aluno/alterar_aluno.php
+++ application/views/aluno/alterar_aluno.php
@@ -22,8 +22,7 @@
     if(isset($aluno)) {
         if ($aluno != null) {
 
-
-            if ($aluno['status_ativo']==1) {
+            if ($aluno['status_ativo'] == 1) {
 
                 echo form_open("aluno/alteraraluno", $atributos);
                 echo form_hidden('id_aluno', $aluno['id_aluno']);
@@ -77,9 +76,7 @@
                 echo "</div>";
                 echo "</div>";
                 echo form_close();
-            }
-
-            else{
+            } else {
 
                 echo form_open("aluno/alteraraluno", $atributos);
                 echo form_hidden('id_aluno', $aluno['id_aluno']);
@@ -130,67 +127,8 @@
                 echo anchor("aluno/alterar_aluno/", "Voltar", 'class = "btn btn-default"');
             }
 
-        }else{echo "ERRO";}
-
-            echo form_open("aluno/alteraraluno", $atributos);
-            echo form_hidden('id_aluno', $aluno['id_aluno']);
-            echo "<div class='row'>";
-            echo form_label("Número de matrícula", "matricula", array("class" => "col-sm-2 control-label"));
-            echo "<div class='form-group col-sm-2'>";
-            echo form_input(array("name" => "matricula", "value" => set_value("matricula", $aluno['cd_mat_aluno']), "required" => "required", "id" => "matricula", "class" => "form-control", "maxlength" => "13", "minlength" => "13", "min" => "0", "readonly" => "readonly"));
-            echo form_error("matricula");
-            echo "</div>";
-            echo "<div class='col-sm-1'>";
-            echo "</div>";
-            echo form_label("Turma", "turma", array("class" => "col-sm-2 control-label"));
-            echo "<div class='form-group  col-sm-3'>";
-            echo form_dropdown('turma', $dropDownTurma, set_value("turma", $aluno['id_turma']), array("class" => "form-control"));
-            echo form_error("turma");
-            echo "</div>";
-            echo "</div>";
-
-            echo "<div class='row'>";
-            echo form_label("Nome Completo", "nomeCompleto", array("class" => "col-sm-2 control-label"));
-            echo "<div class='form-group col-sm-3'>";
-            echo form_input(array("name" => "nome", "value" => set_value("nome", $aluno['nm_aluno']), "required" => "required", "id" => "nomeCompleto", "class" => "form-control", "maxlength" => "70"));
-            echo form_error("nome");
-            echo "</div>";
-            echo form_label("Telefone Residencial", "telefone", array("class" => "col-sm-2 control-label"));
-            echo "<div class='form-group col-sm-2'>";
-            echo form_input(array("name" => "telefone", "value" => set_value("telefone", $aluno['cd_tel_residencial']), "id" => "telefone", "class" => "form-control phone-mask", "pattern" => ".{10}|.{11,}", "required title" => "O número de telefone deve conter entre 10 e 11 digitos" ,"maxlength" => "15"));
-            echo form_error("telefone");
-            echo "</div>";
-            echo "</div>";
-
-
-            echo "<div class='row'>";
-            echo form_label("Email", "email", array("class" => "col-sm-2 control-label"));
-            echo "<div class='form-group col-sm-3'>";
-            echo form_input(array("name" => "email", "value" => set_value("email", $aluno['nm_email']), "required" => "required", "type" => "email", "id" => "email", "class" => "form-control", "maxlength" => "70"));
-            echo form_error("email");
-            echo "</div>";
-            echo form_label("Telefone Celular", "celular", array("class" => "col-sm-2 control-label"));
-            echo "<div class='form-group col-sm-2'>";
-            echo form_input(array("name" => "celular", "value" => set_value("celular", $aluno['cd_tel_celular']), "id" => "celular", "class" => "form-control phone-mask", "pattern" => ".{10}|.{11,}", "required title" => "O número de celular deve conter entre 10 e 11 digitos" ,"maxlength" => "15"));
-            echo form_error("celular");
-            echo "</div>";
-            echo "</div>";
-
-            echo "<div class='row'>";
-            echo "<div class='form-group'>";
-            echo "<div class='col-sm-offset-2 col-sm-10'>";
-            echo anchor("temporario/administrador","Cancelar", array("class" => "btn btn-default"));
-            echo "        ";
-            echo form_button(array("class" => "btn btn-default", "content" => "Salvar", "type" => "submit"));
-            echo "</div>";
-            echo "</div>";
-            echo "</div>";
-            echo form_close();
-
-        }else{
-
-        echo "ERRO";
+        } else {
+            echo "ERRO";
         }
-
-
+    }
 ?>
