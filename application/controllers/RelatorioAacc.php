9a1417e0f3b5a75f63c798f7937938fb5b9b0b95
KarinaDatore <karina.datore@fatec.sp.gov.br>
2016-10-23 13:12:41 -0200

    Alteração RelatorioAacc

--- application/controllers/RelatorioAacc.php
+++ application/controllers/RelatorioAacc.php
@@ -1,9 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+class RelatorioAacc extends CI_Controller{
 
-class RelatorioAacc
-{
+    public function index(){
+        $this->load->template_admin("aluno/validacao_relatorio_aluno");
+    }
 
+
+    public function validacao_relatorio_aluno(){
+
+        $this->load->template_admin("aluno/validacao_relatorio_aluno");
+
+    }
 
 
 }
