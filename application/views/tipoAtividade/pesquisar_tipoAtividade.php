34aef1b445c4dd56df794a6e3476db957febee23
victorcsandrade <victorcsandrade@gmail.com>
2016-11-08 14:36:13 -0200

    Tipo Atividade Pesquisa Hr

--- application/views/tipoAtividade/pesquisar_tipoAtividade.php
+++ application/views/tipoAtividade/pesquisar_tipoAtividade.php
@@ -1,23 +1,26 @@
 
 <?= form_fieldset("<h1>Pesquisa de Tipo de Atividade</h1>") ?>
-<table class='table-striped'>
+
+<table class='table'>
 
     <thead>
-        <tr>
-            <th>Tipo de Atividade</th>
-            <?php if(isset($id_tipo_atividade)) echo "<th></th>"; ?>
-        </tr>
+    <tr>
+        <th>Tipo de Atividade</th>
+        <th>Horas</th>
+        <?php if(isset($id_tipo_atividade)) echo "<th></th>"; ?>
+    </tr>
     </thead>
     <tbody>
 
-        <?php foreach($tiposAtividades as $tipoAtividade): ?>
+    <?php foreach($tiposAtividades as $tipoAtividade): ?>
 
-            <tr>
-                <td><?= $tipoAtividade['nm_tipo_atividade'] ?></td>
-                <?php if(isset($id_tipo_atividade)) echo "<td>".anchor("tipoAtividade/alterarTipoAtividade/{$tipoAtividade['id_tipo_atividade']}", "Alterar", 'class = "btn btn-default"')."</td>"; ?>
-            </tr>
+        <tr>
+            <td><?= $tipoAtividade['nm_tipo_atividade'] ?></td>
+            <td><?= $tipoAtividade['qt_estimada_horas_atividade'] ?></td>
+            <?php if(isset($id_tipo_atividade)) echo "<td>".anchor("tipoAtividade/alterarTipoAtividade/{$tipoAtividade['id_tipo_atividade']}", "Alterar", 'class = "btn btn-default"')."</td>"; ?>
+        </tr>
 
-        <?php endforeach; ?>
+    <?php endforeach; ?>
 
     </tbody>
 
