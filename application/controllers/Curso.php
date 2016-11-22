691666d85b92e45a631a6243e927c13e0ba1fd5f
adilson <adilsonbarbosa.it@gmail.com>
2016-11-22 14:38:08 -0200

    ajustes no Cadastro de curso

--- application/controllers/Curso.php
+++ application/controllers/Curso.php
@@ -3,26 +3,30 @@
 class curso extends CI_Controller
 {
     public function index(){
-
+        autoriza(2);
         $this->load->template_admin("curso/cadastro_curso.php");
-
     }
 
     public function cadastro_curso(){
-
-        $this->load->template_admin("curso/cadastro_curso.php");
-
+        autoriza(2);
+        $this->load->model("unidade_model");
+        $unidades = $this->unidade_model->dropDownUnidade();
+        $dados = array("unidades" => $unidades);
+        $this->load->template_admin("curso/cadastro_curso.php",$dados);
     }
 
     public function cadastrarCurso(){
-
+        autoriza(2);
         if($this->_validaFormulario()) {
 
             $usuarioLogado = $this->session->userdata("usuario_logado");
-
             $curso = array(
-                "nm_curso" => $this->input->post("nm_curso"),
-                "nm_abreviacao" => $this->input->post("nm_abreviacao"),
+                "id_unidade" => $this->input->post("Unidade"),
+                "nm_curso" => $this->input->post("nome_curso"),
+                "nm_abreviacao" => $this->input->post("abreviacao_curso"),
+                "nm_coordenador_curso" => $this->input->post("nome_coordenador"),
+                "cd_cpf_coordenador_curso" => $this->input->post("cpf_coordenador"),
+                "qt_horas_aacc" => $this->input->post("qtd_horas_aacc"),
                 "id_user_adm_cadastrou" => $usuarioLogado['id_usuario'],
                 "dt_cadastro" => mdate("%Y-%m-%d %H:%i:%s", time())
             );
@@ -32,8 +36,10 @@ class curso extends CI_Controller
 
             $this->session->set_flashdata("success", "Cadastro efetuado com sucesso!");
             redirect('/curso/cadastro_curso');
+        }else{
+            redirect("/curso/cadastro_curso");
         }
-        $this->load->template_admin("curso/cadastro_curso");
+            $this->load->template_admin("curso/cadastro_curso");
     }
 
     public function buscaCursosByUnidade(){
@@ -57,11 +63,12 @@ class curso extends CI_Controller
             'is_unique' => 'Já existe um curso cadastrado com esta abreviação.'
         );
         
-        $this->form_validation->set_rules("nm_abreviacao", "nm_abreviacao", "required|max_length[10]|is_unique[tb_curso.nm_abreviacao]", $mensagem);
-        $this->form_validation->set_rules("nm_curso", "nm_curso", "required|max_length[70]");
-
+        $this->form_validation->set_rules("abreviacao_curso", "abreviacao_curso", "required|max_length[10]|is_unique[tb_curso.nm_abreviacao]", $mensagem);
+        $this->form_validation->set_rules("nome_curso", "nome_curso", "required|max_length[70]");
+        $this->form_validation->set_rules("nome_coordenador", "nome_coordenador", "required|max_length[100]");
+        $this->form_validation->set_rules("cpf_coordenador", "cpf_coordenador", "required|max_length[50]|numeric");
+        $this->form_validation->set_rules("qtd_horas_aacc", "qtd_horas_aacc", "required|numeric");
         $this->form_validation->set_error_delimiters('<p class="alert-danger">', '</p>');
-
         return $this->form_validation->run();//roda regra
     }
 
--- application/views/curso/cadastro_curso.php
+++ application/views/curso/cadastro_curso.php
@@ -4,21 +4,59 @@ echo form_fieldset("<h1>Cadastro de Curso</h1>");
 echo form_open('curso/cadastrarCurso', 'class = form-horizontal');
 
 echo "<div class='row'>";
-echo form_label("Abreviação do Curso", "nm_abreviacao", array("class" => "col-md-2 control-label"));
-echo "<div class='form-group col-md-2'>";
-echo form_input(array("name" => "nm_abreviacao", "value" => set_value("nm_abreviacao",""), "id" => "nm_abreviacao" ,"class" => "form-control", "maxlength" => "10"));
-echo form_error("nm_abreviacao");
+echo form_label("Unidade", "unidade", array("class" => "col-md-2 control-label"));
+echo "<div class='form-group col-md-4'>";
+$unidades = array('' =>  "Selecione")+$unidades;
+if (!isset($unidade)) {
+    echo form_dropdown('Unidade', $unidades, "", array("class" => "form-control", 'id' => 'unidade', 'required' => 'required'));
+
+} else {
+    echo form_dropdown('Unidade', $unidades, $unidade, array("class" => "form-control", 'id' => 'unidade', 'required' => 'required'));
+
+}
+echo "</div>";
+echo "</div>";
+
+
+echo "<div class='row'>";
+echo form_label("Nome do Curso", "nome_curso", array("class" => "col-md-2 control-label"));
+echo "<div class='form-group col-md-6'>";
+echo form_input(array("name" => "nome_curso", "value" => set_value("nome_curso",""), "id" => "nome_curso" ,"class" => "form-control", "maxlength" => "70"));
+echo form_error("nome_curso");
 echo "</div>";
 echo "</div>";
 
 echo "<div class='row'>";
-echo form_label("Nome do Curso", "nm_curso", array("class" => "col-md-2 control-label"));
-echo "<div class='form-group col-md-4'>";
-echo form_input(array("name" => "nm_curso", "value" => set_value("nm_curso",""), "id" => "nm_curso" ,"class" => "form-control", "maxlength" => "70"));
-echo form_error("nm_curso");
+echo form_label("Abreviação do Curso", "abreviacao_curso", array("class" => "col-md-2 control-label"));
+echo "<div class='form-group col-md-2'>";
+echo form_input(array("name" => "abreviacao_curso", "value" => set_value("abreviacao_curso",""), "id" => "abreviacao_curso" ,"class" => "form-control", "maxlength" => "10"));
+echo form_error("abreviacao_curso");
 echo "</div>";
 echo "</div>";
 
+echo "<div class='row'>";
+echo form_label("Nome do Coordenador", "nome_coordenador", array("class" => "col-md-2 control-label"));
+echo "<div class='form-group col-md-6'>";
+echo form_input(array("name" => "nome_coordenador", "value" => set_value("nome_coordenador",""), "id" => "nome_coordenador" ,"class" => "form-control", "maxlength" => "100"));
+echo form_error("nome_coordenador");
+echo "</div>";
+echo "</div>";
+
+echo "<div class='row'>";
+echo form_label("Cpf do Coordenador", "cpf_coordenador", array("class" => "col-md-2 control-label"));
+echo "<div class='form-group col-md-3'>";
+echo form_input(array("name" => "cpf_coordenador", "value" => set_value("cpf_coordenador",""), "id" => "cpf_coordenador" ,"class" => "form-control", "maxlength" => "50"));
+echo form_error("cpf_coordenador");
+echo "</div>";
+echo "</div>";
+
+echo "<div class='row'>";
+echo form_label("Quantidade de horas do curso de AACC", "qtd_horas_aacc", array("class" => "col-md-2 control-label"));
+echo "<div class='form-group col-md-2'>";
+echo form_input(array("name" => "qtd_horas_aacc", "value" => set_value("qtd_horas_aacc",""), "id" => "qtd_horas_aacc" ,"class" => "form-control","type" => "number"));
+echo form_error("qtd_horas_aacc");
+echo "</div>";
+echo "</div>";
 echo "<div class='row'>";
 echo "<div class='form-group'>";
 echo "<div class='col-md-offset-2 col-md-10'>";
